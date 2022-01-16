4713581107355003e587ac957f7807690c71c8ae
swu.zzm <1620724912@qq.com>
2022-01-16 14:19:21 +0800

    1.层级菜单布局

---
+++ app/src/main/java/com/swu/zzm/java72/MainActivity.java
@@ -0,0 +1,34 @@
+package com.swu.zzm.java72;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
